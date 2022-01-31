270fa8045aa54c36184f2d986871ab297cb700d4
Chris Hannaby <chris.hannaby@netlify.com>
2022-01-31 15:50:01 -0700

    add forms

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,12 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-6">
+    <main className="py-12 px-6 container mx-auto prose">
       <Head>
         <title>Next.js + Netlify</title>
       </Head>
       <p>
-        <a
-          className="underline"
-          href="https://github.com/lukePeavey/quotable#list-quotes"
-        >
+        <a href="https://github.com/lukePeavey/quotable#list-quotes">
           Full list of options here
         </a>
       </p>
